69de57b3e90f68a3ef36c9443624c461810c94a9
Evgeny Vinnik <evvinnik@microsoft.com>
2018-05-03 21:47:14 -0700

    Even more training

--- Codility2/Program.cs
+++ Codility2/Program.cs
@@ -10,30 +10,142 @@ namespace Codility2
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine(TapeEquilibrium(new int[] { -1000,1000, 3 }).ToString());
+            //Console.WriteLine(TapeEquilibrium(new int[] { 3, 1, 2, 4, 3 }).ToString());
+            //Console.WriteLine(FrogRiverOne(5, new int[] { 1, 3, 1, 4, 2, 3,  4 , 5}).ToString());
+            Console.WriteLine(MissingInteger(new int[] { 1, 3, 6, 4, 1, 2 }).ToString());
+            Console.WriteLine(MissingInteger(new int[] { -1, -3 }).ToString());
+            Console.WriteLine(MissingInteger(new int[] { 1, 2, 3 }).ToString());
+            Console.WriteLine(MissingInteger(new int[] { -1, 0, 1, 2, 3 }).ToString());
+            Console.WriteLine(MissingInteger(new int[] { 0 }).ToString());
+            Console.WriteLine(MissingInteger(new int[] { 1 }).ToString());
+            Console.WriteLine(MissingInteger(new int[] { 1000001 }).ToString());
+            //Console.WriteLine(PermCheck(new int[] { 1, 2 }).ToString());
+            //Console.WriteLine(FrogJmp(10, 85, 30).ToString());
             //Console.WriteLine(Odd(new int[] { 9, 3, 9, 3, 9, 7, 9 }).ToString());
-            int[] A = new int[999999];
-            for(int i = 0; i < A.Length; i++)
-            {
-                A[i] = i + 2;
-            }
-            Console.WriteLine(MissingElement(A).ToString());
+            //int[] A = new int[999999];
+            //for(int i = 0; i < A.Length; i++)
+            //{
+            //    A[i] = i + 2;
+            //}
+            //Console.WriteLine(MissingElement(A).ToString());
             //CyclicRotation(new int[] { }, 5, 6);
             //Console.WriteLine(Program.BinaryGap(328).ToString());
         }
 
-        public static int MissingElement(int[] A)
+        public static int MissingInteger(int[] A)
         {
-            if (A == null || A.Length == 0)
-                return 1;
+            byte[] check = new byte[A.Length];
 
-            double sum = ( (A.Length + 1.0) * ((A.Length + 1.0) + 1.0) )/ 2.0;
+            int max = 0;
 
-            foreach(int n in A)
+            foreach (var n in A)
             {
-                sum -= n;
+                if (n > check.Length)
+                    continue;
+                if (n > 0)
+                {
+                    check[n - 1] = 1;
+                }
+
+                if (n > max)
+                    max = n;
             }
-            return (int)sum;
+
+            for(int i = 0; i <check.Length; i++)
+            {
+                if (check[i] == 0)
+                    return i + 1;
+            }
+
+            return max + 1;
         }
+
+        //public static int FrogRiverOne(int X, int[] A)
+        //{
+        //    HashSet<int> steps = new HashSet<int>();
+        //    for (int i = 1; i <= X; i++)
+        //    {
+        //        steps.Add(i);
+        //    }
+
+        //    for (int i = 0; i < A.Length; i++)
+        //    {
+        //        if (steps.Contains(A[i]))
+        //            steps.Remove(A[i]);
+        //        if (steps.Count == 0)
+        //        {
+        //            return i;
+        //        }
+        //    }
+
+        //    return -1;
+        //}
+        //public static int PermCheck(int[] A)
+        //{
+        //    byte[] check = new byte[A.Length];
+
+        //    foreach (int n in A)
+        //    {
+        //        if (n > A.Length)
+        //            return 0;
+
+        //        check[n - 1] += 1;
+
+        //        if (check[n - 1] > 2)
+        //            return 0;
+        //    }
+
+        //    foreach (byte n in check)
+        //    {
+        //        if (n == 0)
+        //            return 0;
+        //    }
+
+        //    return 1;
+        //}
+
+        //public static int TapeEquilibrium(int[] A)
+        //{
+        //    int sum = 0;
+        //    for (int i = 1; i < A.Length; i++)
+        //    {
+        //        sum += A[i];
+        //    }
+
+        //    int right_sum = A[0];
+        //    int min = Math.Abs(sum - right_sum);
+
+        //    for (int i = 1; i < A.Length-1; i++)
+        //    {
+        //        right_sum += A[i];
+        //        sum -= A[i];
+
+        //        if (Math.Abs(sum - right_sum) < min )
+        //        {
+        //            min = Math.Abs(sum - right_sum);
+        //        }
+        //    }
+
+        //    return min;
+        //}
+        //public static int FrogJmp(int X, int Y, int D)
+        //{
+        //    return (int)(Math.Ceiling((double) (Y - X) / (double) D));
+        //}
+        //public static int MissingElement(int[] A)
+        //{
+        //    if (A == null || A.Length == 0)
+        //        return 1;
+
+        //    double sum = ( (A.Length + 1.0) * ((A.Length + 1.0) + 1.0) )/ 2.0;
+
+        //    foreach(int n in A)
+        //    {
+        //        sum -= n;
+        //    }
+        //    return (int)sum;
+        //}
 
         //public static int Odd(int []A)
         //{
