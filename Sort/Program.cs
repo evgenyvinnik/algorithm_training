1fdfe7eae62cd37038fa3ea30a4b9bd9961c88be
Evgeny Vinnik <evvinnik@microsoft.com>
2018-05-19 18:57:51 -0700

    Implemented sorting algorithms.

--- Sort/Program.cs
+++ Sort/Program.cs
@@ -21,29 +21,88 @@ namespace Sort
 
         static void Main(string[] args)
         {
-            const int arraySize = 1009;
+            const int arraySize = 109;
             int[] A = new int [arraySize];
             Random rnd = new Random();
-            for(int i = 0; i < arraySize; i++)
+            for (int i = 0; i < arraySize; i++)
             {
-                A[i] = rnd.Next(10000);
+                A[i] = rnd.Next(100);
             }
 
-           // A = new int[]{3,7,8,5,2,1,9,5,4};
+            // A = new int[]{3,7,8,5,2,1,9,5,4};
 
-            var B = A;
-            PrintArray(B);
+            PrintArray(A);
+            {
+                int[] B = new int[A.Length];
+                A.CopyTo(B, 0);
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                QuickSort(ref B);
+                watch.Stop();
+                var elapsedMs = watch.ElapsedTicks;
+                Console.WriteLine($"QuickSort Elapsed ticks {elapsedMs}");
+                PrintArray(B);
+            }
+            {
+                int[] C = new int[A.Length];
+                A.CopyTo(C, 0);
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                BubbleSort(ref C);
+                watch.Stop();
+                var elapsedMs = watch.ElapsedTicks;
+                Console.WriteLine($"BubbleSort Elapsed ticks {elapsedMs}");
+                PrintArray(C);
+            }
 
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            // the code that you want to measure comes here
-            watch.Stop();
-            var elapsedMs = watch.ElapsedTicks;
-            QuickSort(ref B);
-            Console.WriteLine($"Elapsed ticks {elapsedMs}");
-            PrintArray(B);
+            {
+                int[] D = new int[A.Length];
+                A.CopyTo(D, 0);
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                MergeSort(ref D);
+                watch.Stop();
+                var elapsedMs = watch.ElapsedTicks;
+                Console.WriteLine($"MergeSort Elapsed ticks {elapsedMs}");
+                PrintArray(D);
+            }
+
+            {
+                int[] E = new int[A.Length];
+                A.CopyTo(E, 0);
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                HeapSort(ref E);
+                watch.Stop();
+                var elapsedMs = watch.ElapsedTicks;
+                Console.WriteLine($"HeapSort Elapsed ticks {elapsedMs}");
+                PrintArray(E);
+            }
+
+            {
+                int[] F= new int[A.Length];
+                A.CopyTo(F, 0);
+                List<int> FList = F.ToList();
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                FList.Sort();
+                watch.Stop();
+                var elapsedMs = watch.ElapsedTicks;
+                Console.WriteLine($"Built-in sort Elapsed ticks {elapsedMs}");
+                PrintArray(FList);
+            }
         }
 
-
+        static void BubbleSort(ref int[] b)
+        {
+            for (int i = 0; i < b.Length; i++)
+            {
+                for (int j = 0; j < b.Length; j++)
+                {
+                    if (j != i && b[j] > b[i])
+                    {
+                        int c = b[i];
+                        b[i] = b[j];
+                        b[j] = c;
+                    }
+                }
+            }
+        }
 
         static void QuickSort(ref int[] b)
         {
@@ -71,7 +130,7 @@ namespace Sort
                 if (b[j] < pivot)
                 {
                     i = i + 1;
-                    if (i + 1 != j)
+                    if (i != j)
                     {
                         c = b[i];
                         b[i] = b[j];
@@ -85,6 +144,86 @@ namespace Sort
             b[hi] = c;
 
             return i + 1;
+        }
+
+
+        static void MergeSort(ref int[] a)
+        {
+            int[] b = new int[a.Length];
+            a.CopyTo(b, 0);
+            TopDownSplitMerge(ref b, 0, a.Length, ref a);
+        }
+
+        static void TopDownSplitMerge(ref int[] b, int begin, int end, ref int[] a)
+        {
+            if (end - begin < 2)
+                return;
+
+            int middle = (begin + end) / 2;
+
+            TopDownSplitMerge(ref a, begin, middle, ref b);
+            TopDownSplitMerge(ref a, middle, end, ref b);
+
+            TopDownMerge(ref b, begin, middle, end, ref a);
+        }
+
+        static void TopDownMerge(ref int[] a, int begin, int middle,  int end, ref int[] b)
+        {
+            int i = begin;
+            int j = middle;
+
+            for (int k = begin; k < end; k++)
+            {
+                if (i < middle && (j >= end || a[i] <= a[j]))
+                {
+                    b[k] = a[i];
+                    i++;
+                }
+                else
+                {
+                    b[k] = a[j];
+                    j++;
+                }
+            }
+        }
+
+        static void HeapSort(ref int[] b)
+        {
+            for (int i = b.Length / 2 - 1; i >= 0; i--)
+            {
+                heapify(ref b, b.Length, i);
+            }
+
+            for (int i = b.Length - 1; i >= 0; i--)
+            {
+                int c = b[i];
+                b[i] = b[0];
+                b[0] = c;
+
+                heapify(ref b, i, 0);
+            }
+        }
+
+        static void heapify(ref int[] b, int n, int i)
+        {
+            int largest = i;
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+
+            if (l < n && b[l] > b[largest])
+                largest = l;
+
+            if (r < n && b[r] > b[largest])
+                largest = r;
+
+            if (largest != i)
+            {
+                int c = b[i];
+                b[i] = b[largest];
+                b[largest] = c;
+
+                heapify(ref b, n, largest);
+            }
         }
     }
 }
