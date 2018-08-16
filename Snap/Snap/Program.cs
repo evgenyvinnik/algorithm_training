076d62a14582972e6acef36f70aee812473fa348
Evgeny Vinnik <evvinnik@microsoft.com>
2018-08-15 23:41:37 -0700

    Added more training.

---
+++ Snap/Snap/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snap
+{
+    class Program
+    {
+        class Tree
+        {
+            public Tree left;
+            public Tree right;
+            public int data;
+
+            public Tree(int data) : this(data, null, null)
+            {
+
+            }
+
+            public Tree(int data, Tree left, Tree right)
+            {
+                this.data = data;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        //        1
+        //     2   3
+        //   4   5    6
+        // 4 5 2 6 3 1
+        //
+        static void Main(string[] args)
+        {
+            Tree tree4 = new Tree(4);
+            Tree tree5 = new Tree(5);
+            Tree tree6 = new Tree(6);
+
+            Tree tree2 = new Tree(2, tree4, tree5);
+            Tree tree3 = new Tree(3, null, tree6);
+
+            Tree root = new Tree(1, tree2, tree3);
+
+            List<int> res = postfix(root);
+
+            for(int i = res.Count-1; i>=0; i--)
+            {
+                Console.WriteLine(res[i]);
+            }
+        }
+
+        static List<int> postfix(Tree root)
+        {
+            List<int> res = new List<int>();
+
+            Stack<Tree> stack = new Stack<Tree>();
+            stack.Push(root);
+
+            while(stack.Count > 0)
+            {
+                Tree p = stack.Pop();
+                if(p.left != null)
+                {
+                    stack.Push(p.left);
+                }
+
+                if (p.right != null)
+                {
+                    stack.Push(p.right);
+                }
+
+                res.Add(p.data);
+            }
+
+            return res;
+        }
+    }
+}
---
+++ TTDTest/TTDTest/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTDTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //RLE encoding
+            //bitmap image
+            //0s and 1s
+            //000011 -> 0:4 1:2
+
+            //2d array of ones and zeros
+            // return compressed version
+
+            var bitmap = new int[,]
+            {
+                { 1, 0 , 1, 1},
+                { 1, 1 , 1, 1},
+                { 0, 1 , 1, 0},
+            };
+
+            // output
+            // 0:2, 1:2
+            // 0:1, 1:3
+            // 0:1, 1:2, 0:1
+
+            // output
+            // 0:2, 1:6, 0:1, 1:2, 0:1
+            // 
+
+            var compressed = RleCompression(bitmap);
+
+            if (compressed[0] == 0)
+                compressed.RemoveAt(0);
+
+            foreach(int val in compressed)
+            {
+                Console.WriteLine(val);
+            }
+        }
+        
+        static List<int> RleCompression(int[,] bitmap)
+        {
+            int prev = 0;
+            List<int> res = new List<int>() { 0 };
+
+
+            for (int i = 0; i < bitmap.GetLength(0); i++)
+            {
+                for (int j = 0; j < bitmap.GetLength(1); j++)
+                {
+                    if(bitmap[i, j] == prev)
+                    {
+                        res[res.Count - 1]++;
+                    }
+                    else
+                    {
+                        res.Add(1);
+                        prev = bitmap[i, j];
+                    }
+                }
+            }
+
+            return res;
+        }
+
+    }
+}
