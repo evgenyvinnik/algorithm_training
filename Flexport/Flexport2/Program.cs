24a5350f5dab33c7ec3b0dde762ac2be080283b6
Evgeny Vinnik <evvinnik@microsoft.com>
2018-09-13 23:11:23 -0700

    More Training.

---
+++ Flexport/Flexport2/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flexport2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<int>> stacks = new List<List<int>>();
+            stacks.Add(new List<int>());
+
+            Push(ref stacks, 1);
+            Push(ref stacks, 2);
+
+            Begin(ref stacks);
+            Console.WriteLine($"{Pop(ref stacks)}");
+            Push(ref stacks, 3);
+            Console.WriteLine($"{Peek(stacks)}");
+            RollBack(ref stacks);
+
+            Console.WriteLine($"{Peek(stacks)}");
+
+            Begin(ref stacks);
+            Push(ref stacks, 4);
+                Begin(ref stacks);
+                Push(ref stacks, 5);
+                Commit(ref stacks);
+            Console.WriteLine($"{Peek(stacks)}");
+            RollBack(ref stacks);
+
+            Console.WriteLine($"{Peek(stacks)}");
+
+            //RollBack(ref stacks);//should return error
+        }
+
+        static int Pop(ref List<List<int>> stacks)
+        {
+            if(stacks.Count == 0)
+            {
+                throw new Exception();
+            }
+            var topStack = stacks[stacks.Count - 1];
+
+            if (topStack.Count == 0)
+            {
+                throw new Exception();
+            }
+            var val = topStack[topStack.Count - 1];
+
+            topStack.RemoveAt(topStack.Count - 1);
+
+            return val;
+        }
+
+        static void Push(ref List<List<int>> stacks, int val)
+        {
+            if (stacks.Count == 0)
+            {
+                throw new Exception();
+            }
+
+            var topStack = stacks[stacks.Count - 1];
+            topStack.Add(val);
+        }
+
+        static int Peek(List<List<int>> stacks)
+        {
+            if (stacks.Count == 0)
+            {
+                throw new Exception();
+            }
+
+            var topStack = stacks[stacks.Count - 1];
+            if (topStack.Count == 0)
+            {
+                throw new Exception();
+            }
+            var val = topStack[topStack.Count - 1];
+            return val;
+        }
+
+        static void Begin(ref List<List<int>> stacks)
+        {
+            if (stacks.Count == 0)
+            {
+                throw new Exception();
+            }
+
+            var topStack = stacks[stacks.Count - 1];
+
+            stacks.Add(new List<int>(topStack));
+        }
+
+        static void RollBack(ref List<List<int>> stacks)
+        {
+            if (stacks.Count <= 1)
+            {
+                throw new Exception();
+            }
+
+            stacks.RemoveAt(stacks.Count - 1);
+        }
+
+
+        static void Commit(ref List<List<int>> stacks)
+        {
+            if (stacks.Count <=2)
+            {
+                throw new Exception();
+            }
+
+            var topStack = stacks[stacks.Count - 1];
+            stacks.RemoveAt(stacks.Count - 1);
+
+            var replaceStack = stacks[stacks.Count - 1];
+            stacks.RemoveAt(stacks.Count - 1);
+
+            stacks.Add(new List<int>(topStack));
+        }
+    }
+}
---
+++ Teams/Teams2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teams2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string time1 = "1:45:10 PM";// 13:45:10
+            //12AM -> 00:00
+            //12PM -> 12
+
+            string formattedTime = format24HR(time1);
+            Console.WriteLine($"{formattedTime}");
+
+            time1 = "12:10:10 AM"; // 00:10:10
+            formattedTime = format24HR(time1);
+            Console.WriteLine($"{formattedTime}");
+
+            time1 = "12:10:10 PM"; // 12:10:10
+            formattedTime = format24HR(time1);
+            Console.WriteLine($"{formattedTime}");
+
+            time1 = "12:10:10 PM"; // 12:10:10
+            formattedTime = format24HR(time1);
+            Console.WriteLine($"{formattedTime}");
+
+            Dictionary<string, string> timeConvert = new Dictionary<string, string>();
+            timeConvert.Add("12:10:10 AM", "00:10:10")
+        }
+
+        static string format24HR(string time)
+        {
+            if (time == null)
+                return null;
+
+            //[12]:[12]:[12]_"AM"/"PM"; 
+
+            int h = 0;
+            int m = 0;
+            int s = 0;
+
+            int add12 = 0;
+
+            var timeSplit = time.Split(' ');
+            var components = timeSplit[0].Split(':');
+
+            h = Int32.Parse(components[0]);
+            m = Int32.Parse(components[1]);
+            s = Int32.Parse(components[2]);
+
+            if (timeSplit[1] == "PM" && h != 12)//edge cases
+            {
+                add12 = 12;
+            }
+            else if (timeSplit[1] == "AM" && h == 12)
+            {
+                add12 = -12;
+            }
+
+            h += add12;
+
+            return $"{h}:{m}:{s}";
+        }
+    }
+}
