be2217930f31ea0e5b70039c474b7a2595333621
Evgeny Vinnik <evvinnik@microsoft.com>
2018-09-21 12:17:59 -0700

    added new training samples

---
+++ TrieContacts/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TrieContacts
+{
+    class Program
+    {
+        /*
+             * Complete the contacts function below.
+             */
+        static int[] contacts(string[][] queries)
+        {
+            List<int> res = new List<int>();
+            /*
+             * Write your code here.
+             */
+            return res.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int queriesRows = Convert.ToInt32(Console.ReadLine());
+
+            string[][] queries = new string[queriesRows][];
+
+            for (int queriesRowItr = 0; queriesRowItr < queriesRows; queriesRowItr++)
+            {
+                queries[queriesRowItr] = Console.ReadLine().Split(' ');
+            }
+
+            int[] result = contacts(queries);
+
+            Console.WriteLine(string.Join("\n", result));
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
